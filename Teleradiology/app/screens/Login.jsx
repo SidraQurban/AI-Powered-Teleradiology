1a39dbe344cebfb849ed9c8a9aebe00c97157325
Sidra Qurban <100536445+SidraQurban@users.noreply.github.com>
2025-09-11 11:10:54 -0700

    ./

--- Teleradiology/app/screens/Login.jsx
+++ Teleradiology/app/screens/Login.jsx
@@ -33,12 +33,6 @@ const Login = () => {
           <Text>O</Text>
           <Text>O</Text>
           <Text>O</Text>
-          <View
-            style={{
-              height: responsiveHeight(100),
-              backgroundColor: "#f8f9fa",
-            }}
-          ></View>
         </View>
       </View>
     </SafeAreaView>
