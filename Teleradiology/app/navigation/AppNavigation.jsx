d1b8e66387f767fa5697de790865a948d04b821e
Sidra Qurban <100536445+SidraQurban@users.noreply.github.com>
2025-09-11 10:12:39 -0700

    updated

--- Teleradiology/app/navigation/AppNavigation.jsx
+++ Teleradiology/app/navigation/AppNavigation.jsx
@@ -1,6 +1,7 @@
 import { View, Text } from "react-native";
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
+import WelcomeScreen from "../screens/WelcomeScreen";
 
 const Stack = createNativeStackNavigator();
 const AppNavigation = () => {
