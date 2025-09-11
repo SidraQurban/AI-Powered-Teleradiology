e3dd4f125e94005a7eab3c2071f74873d5cd55fb
Sidra Qurban <100536445+SidraQurban@users.noreply.github.com>
2025-09-11 10:50:15 -0700

    ./

--- Teleradiology/app/screens/WelcomeScreen.jsx
+++ Teleradiology/app/screens/WelcomeScreen.jsx
@@ -1,18 +1,30 @@
 import { View, Text } from "react-native";
 import React from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
+import {
+  responsiveFontSize,
+  responsiveHeight,
+} from "react-native-responsive-dimensions";
 
 const WelcomeScreen = () => {
   return (
     <SafeAreaView>
       <View
         style={{
-          flex: 1,
-          height: "100%",
-          backgroundColor: "black",
+          height: responsiveHeight(100),
+          backgroundColor: "#f8f9fa",
         }}
       >
-        <Text>WelcomeScreen</Text>
+        <View
+          style={{
+            marginTop: responsiveHeight(70),
+            alignItems: "center",
+          }}
+        >
+          <Text style={{ fontSize: responsiveFontSize(2), fontWeight: "bold" }}>
+            AI That Cares for Your Health💙🩺
+          </Text>
+        </View>
       </View>
     </SafeAreaView>
   );
