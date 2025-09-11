c6139fd07353ab69ab85a729ba47065ad71b7fbd
Sidra Qurban <100536445+SidraQurban@users.noreply.github.com>
2025-09-11 10:42:49 -0700

    ./

--- Teleradiology/app/screens/WelcomeScreen.jsx
+++ Teleradiology/app/screens/WelcomeScreen.jsx
@@ -8,8 +8,7 @@ const WelcomeScreen = () => {
       <View
         style={{
           flex: 1,
-          justifyContent: "center",
-          alignItems: "center",
+          height: "100%",
           backgroundColor: "black",
         }}
       >
