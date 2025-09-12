2828e07547296d7af717b7c7e6f2ab0573a02cf1
Sidra Qurban <100536445+SidraQurban@users.noreply.github.com>
2025-09-12 11:10:49 -0700

    updated

--- Teleradiology/app/screens/WelcomeScreen.jsx
+++ Teleradiology/app/screens/WelcomeScreen.jsx
@@ -17,7 +17,7 @@ const WelcomeScreen = () => {
           backgroundColor: "#f8f9fa",
         }}
       >
-        {" "}
+        {" 
         <View
           style={{
             marginTop: responsiveHeight(70),
