66dae66993a72113c45c7191a9353c538e59a010
Sidra Qurban <100536445+SidraQurban@users.noreply.github.com>
2025-09-13 11:25:15 -0700

    ./

--- Teleradiology/app/screens/WelcomeScreen.jsx
+++ Teleradiology/app/screens/WelcomeScreen.jsx
@@ -8,7 +8,7 @@ import {
 import { useNavigation } from "@react-navigation/native";
 
 const WelcomeScreen = () => {
-  const sta
+  const st
   const navigation = useNavigation();
   return (
     <SafeAreaView>
