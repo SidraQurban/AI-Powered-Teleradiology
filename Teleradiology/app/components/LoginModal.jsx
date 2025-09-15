67867c9c765d40ccc71f8c858c0bf8c1c420e34e
Sidra Qurban <100536445+SidraQurban@users.noreply.github.com>
2025-09-15 11:18:52 -0700

    ./

--- Teleradiology/app/components/LoginModal.jsx
+++ Teleradiology/app/components/LoginModal.jsx
@@ -1,4 +1,4 @@
-import { View, Text, TextInput } from "react-native";
+import { View, Text, TextInput, TouchableOpacity } from "react-native";
 import React from "react";
 import {
   responsiveFontSize,
@@ -56,6 +56,7 @@ const LoginModal = () => {
                 paddingLeft: responsiveHeight(2),
               }}
             />
+            <TouchableOpacity></TouchableOpacity>
           </View>
         </View>
       </View>
