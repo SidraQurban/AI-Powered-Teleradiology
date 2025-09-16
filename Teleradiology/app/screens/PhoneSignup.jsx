aed570978391ba0bd82db484d3bba77af7dc2a40
Sidra Qurban <100536445+SidraQurban@users.noreply.github.com>
2025-09-16 11:17:37 -0700

    ./

---
+++ Teleradiology/app/screens/PhoneSignup.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const PhoneSignup = () => {
+  return (
+    <View>
+      <Text>PhoneSignup</Text>
+    </View>
+  );
+};
+
+export default PhoneSignup;
