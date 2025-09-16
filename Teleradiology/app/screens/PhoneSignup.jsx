a518eabe1b280d7777ad400dab584e79b8cb8a95
Sidra Qurban <100536445+SidraQurban@users.noreply.github.com>
2025-09-16 11:20:50 -0700

    ./

--- Teleradiology/app/screens/PhoneSignup.jsx
+++ Teleradiology/app/screens/PhoneSignup.jsx
@@ -1,5 +1,13 @@
-import { View, Text } from "react-native";
-import React from "react";
+import { View, Text, TextInput, TouchableOpacity } from "react-native";
+import React, { useState } from "react";
+import {
+  responsiveFontSize,
+  responsiveHeight,
+  responsiveWidth,
+} from "react-native-responsive-dimensions";
+import { Ionicons, MaterialCommunityIcons } from "@expo/vector-icons";
+import { Image } from "react-native";
+import { useNavigation } from "@react-navigation/native";
 
 const PhoneSignup = () => {
   return (
