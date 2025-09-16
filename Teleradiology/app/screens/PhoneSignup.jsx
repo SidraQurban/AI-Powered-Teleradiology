b84cd236a90e02148d55b3a9f6a7469f8d75c15f
Sidra Qurban <100536445+SidraQurban@users.noreply.github.com>
2025-09-16 11:20:31 -0700

    ./

--- Teleradiology/app/screens/PhoneSignup.jsx
+++ Teleradiology/app/screens/PhoneSignup.jsx
@@ -3,9 +3,43 @@ import React from "react";
 
 const PhoneSignup = () => {
   return (
-    <View>
-      <Text>PhoneSignup</Text>
-    </View>
+    <SafeAreaView>
+      <View
+        style={{
+          height: responsiveHeight(100),
+          backgroundColor: "#e9ecef",
+        }}
+      >
+        <View>
+          {/* Header */}
+
+          <View style={{ margin: responsiveHeight(1) }}>
+            <TouchableOpacity
+              onPress={() => navigation.goBack()}
+              style={{
+                height: responsiveHeight(5),
+                width: responsiveHeight(5),
+                borderRadius: responsiveHeight(3),
+                justifyContent: "center",
+                alignItems: "center",
+                backgroundColor: "white",
+              }}
+            >
+              <Ionicons name="chevron-back" size={22} color="#adb5bd" />
+            </TouchableOpacity>
+          </View>
+        </View>
+        <View
+          style={{
+            marginTop: responsiveHeight(-4),
+            alignItems: "center",
+          }}
+        >
+          <Text>LOGO</Text>
+        </View>
+        <SignUpModal />
+      </View>
+    </SafeAreaView>
   );
 };
 
