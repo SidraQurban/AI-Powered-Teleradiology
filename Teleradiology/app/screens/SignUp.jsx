a742b530b1f6ff0286eed7297044635f88ae76e8
Sidra Qurban <100536445+SidraQurban@users.noreply.github.com>
2025-09-16 09:14:46 -0700

    updated

--- Teleradiology/app/screens/SignUp.jsx
+++ Teleradiology/app/screens/SignUp.jsx
@@ -1,11 +1,31 @@
 import { View, Text } from "react-native";
 import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { responsiveHeight } from "react-native-responsive-dimensions";
+import SignUpModal from "../components/SignUpModal";
+import { useNavigation } from "@react-navigation/native";
 
 const SignUp = () => {
+  const navigation = useNavigation();
   return (
-    <View>
-      <Text>SignUp</Text>
-    </View>
+    <SafeAreaView>
+      <View
+        style={{
+          height: responsiveHeight(100),
+          backgroundColor: "#e9ecef",
+        }}
+      >
+        <View
+          style={{
+            marginTop: responsiveHeight(2),
+            alignItems: "center",
+          }}
+        >
+          <Text>LOGO</Text>
+        </View>
+        <SignUpModal />
+      </View>
+    </SafeAreaView>
   );
 };
 
