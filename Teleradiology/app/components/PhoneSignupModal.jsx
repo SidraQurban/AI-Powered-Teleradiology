64ec0ed3858f9d2f8e3c34bb942818a293af8e1f
Sidra Qurban <100536445+SidraQurban@users.noreply.github.com>
2025-09-17 08:41:13 -0700

    ./

--- Teleradiology/app/components/PhoneSignupModal.jsx
+++ Teleradiology/app/components/PhoneSignupModal.jsx
@@ -111,7 +111,7 @@ const PhoneSignupModal = () => {
                 style={{
                   height: responsiveHeight(4),
                   width: responsiveWidth(10),
-                  marginLeft: responsiveWidth(0.1),
+                  marginLeft: responsiveWidth(5),
                   backgroundColor: "#6c757d",
                   position: "absolute",
                   marginTop: responsiveHeight(0.4),
