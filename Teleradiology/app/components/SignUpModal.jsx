fb7b324acf42877c90d8caa54e66a6078236cc89
Sidra Qurban <100536445+SidraQurban@users.noreply.github.com>
2025-10-15 08:33:54 -0700

    UPDATED

--- Teleradiology/app/components/SignUpModal.jsx
+++ Teleradiology/app/components/SignUpModal.jsx
@@ -13,7 +13,7 @@ const SignUpModal = () => {
   const navigation = useNavigation();
   const [showPassword, setShowPassword] = useState(false);
   const [check, setCheck] = useState(false);
-  c;
+
   return (
     <View>
       <View
