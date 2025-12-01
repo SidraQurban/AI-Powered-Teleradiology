13854a4de7ff8e6a09e6293dc86a80784be6fd47
Sidra Qurban <100536445+SidraQurban@users.noreply.github.com>
2025-12-01 13:36:35 +0500

    updated

--- Teleradiology/app/components/Header.jsx
+++ Teleradiology/app/components/Header.jsx
@@ -1,10 +1,31 @@
-import { View, Text } from "react-native";
+import { View, Text, TouchableOpacity } from "react-native";
 import React from "react";
+import { FontAwesome6 } from "@expo/vector-icons";
+import {
+  responsiveHeight,
+  responsiveWidth,
+} from "react-native-responsive-dimensions";
 
 const Header = () => {
   return (
     <View>
       <Text>Header</Text>
+      <View style={{ margin: responsiveHeight(1) }}>
+        <TouchableOpacity
+          style={{
+            height: responsiveHeight(5),
+            width: responsiveHeight(5),
+            borderRadius: responsiveHeight(3),
+            justifyContent: "center",
+            alignItems: "center",
+            backgroundColor: "white",
+            marginLeft: responsiveWidth(85),
+            marginTop: responsiveHeight(-2),
+          }}
+        >
+          <FontAwesome6 name="user" size={25} color="#adb5bd" />
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
--- Teleradiology/app/screens/Home.jsx
+++ Teleradiology/app/screens/Home.jsx
@@ -1,13 +1,14 @@
 import { View, Text } from "react-native";
 import React from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
+import Header from "../components/Header";
+import Carousal from "../components/Carousal";
 
 const Home = () => {
   return (
     <SafeAreaView>
-      {/* Header */}
-
-      {/* Carousel */}
+      <Header />
+      <Carousal />
 
       {/* Recent history */}
 
